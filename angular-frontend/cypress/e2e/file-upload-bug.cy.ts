65fec7faad1e285fc7ede61d04b783e75b576650
fredericbirke <fred@revery-grove.com>
2024-08-20 14:59:03 +0200

    Initial

--- angular-frontend/cypress/e2e/file-upload-bug.cy.ts
+++ angular-frontend/cypress/e2e/file-upload-bug.cy.ts
@@ -2,8 +2,10 @@
 
 describe('template spec', () => {
   const triggerSubscription = `trigger-subscription-button`;
+  const notificationCount = `notification-count`;
   it('passes', () => {
     cy.visit('http://localhost:4200/')
     cy.get(`[data-cy=${triggerSubscription}]`).click()
+    cy.get(`[data-cy=${notificationCount}]`).should('have.text', '1')
   })
 })
